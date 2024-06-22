11cd7ab06d0ba330bc9f57231796938a206360b9
throv <trv.vieira@gmail.com>
2024-06-21 22:50:51 -0300

    new route

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('API está funcionando');
+});
+
 app.use("/api", routes);
 
 app.listen(port, () => {
